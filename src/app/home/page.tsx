ae7f943bb1ce81514ff9239ba45b6cd99741ef71
jeevan <jeevankumar8943@gmail.com>
2025-03-03 12:41:47 +0530

    FEAT : authentication part completed using clerk, ngrok for implement webhook for store the user into db.

---
+++ src/app/home/page.tsx
@@ -0,0 +1,16 @@
+import prisma from "@/lib/db"
+
+
+export default async function Page() {
+    const users = await prisma.user.findMany()
+console.log(users)
+    return (
+   
+           <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+                 <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                  <h1>Home page</h1>
+                 </main>
+          
+               </div>
+    )
+}
